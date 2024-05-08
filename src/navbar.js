fd58de49098aed455ca00b7349a7e213225e738c
abubakrmo <abubakarmufutauoseni@gmail.com>
2024-05-08 04:38:42 +0100

    navbar component created

---
+++ src/navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <div className="navbar">
+            <h2>My blog</h2>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/">Create blog</a>
+            </div>
+        </div>
+    );
+}
+ 
+export default Navbar;
