1827488b87fd55bdc577e19853772bf0f820cdb5
abubakrmo <abubakarmufutauoseni@gmail.com>
2024-05-08 05:18:13 +0100

    delete function added

--- src/blogslist.js
+++ src/blogslist.js
@@ -1,11 +1,13 @@
-const Bloglist = ({blogs}) => {
+const Bloglist = ({blogs, title, handleDelete}) => {
 
     return ( 
         <div className="blogslist">
+            <h1 style={{textAlign: "start"}}>{title}</h1>
             {blogs.map((blog)=>(
                 <div className="blogs" key={blog.id}>
                     <h2 className="blogTitle">{blog.title}</h2>
                     <h4>By {blog.author}</h4>
+                    <button type="button" onClick={()=>handleDelete(blog.id)} style={{marginTop: "10px"}}>Delete</button>
                 </div>
             ))}
         </div>
--- src/homepage.js
+++ src/homepage.js
@@ -1,13 +1,20 @@
 import Bloglist from "./blogslist";
+import { useState } from "react";
 const Homepage = () => {
-    const blogs = [
+    const [blogs, setBlogs] = useState([
         {title: "Running home", author: "Chinua Achebe", id: 1},
         {title: "The sun is falling", author: "Brahim Diaz", id: 2},
         {title: "Save Nigeria", author: "David Hundeyin", id: 3},
-    ]
+    ])
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter((blog)=>blog.id !== id)
+        setBlogs(newBlogs)
+    }
+
     return ( 
         <div className="home">
-            <Bloglist blogs = {blogs}/>
+            <Bloglist blogs = {blogs} title = {"My blog"} handleDelete = {handleDelete}/>
         </div>
      );
 }
