5933bad2a651b32f06c2cfee3d87d452bedbd40f
abubakrmo <abubakarmufutauoseni@gmail.com>
2024-05-16 11:30:47 +0100

    createblog logic created

--- src/createblog.js
+++ src/createblog.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 const CreateBlog = () => {
     const [title, setTitle] = useState('')
     const [body, setBody] = useState('')
-    const [author, setAuthor] = useState('')
+    const [author, setAuthor] = useState('Oglos Danielle')
 
 
     return ( 
@@ -11,16 +11,27 @@ const CreateBlog = () => {
             <h2 style={{textAlign: 'start'}}>Create new blog</h2>
             <form action="">
                 <input 
-
+                type="text" 
+                required
+                value={title}
+                onChange = {e => setTitle(e.target.value)}
                 />
                 <textarea
+                required
+                value={body}
+                onChange = {e => setBody(e.target.value)}
                 ></textarea>
-                <select>
+                <select
+                required
+                value={author}
+                onChange={e => setAuthor(e.target.value)}
+                >
                     <option value="Benjamin Pavard">Benjamin Pavard</option>
                     <option value="Oglos Danielle">Oglos Danielle</option>
                 </select>
                 <button type="submit">Create</button>
             </form>
+            <p>{author}</p>
         </div>
      );
 }
