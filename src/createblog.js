cd0a0d5a562db57d88b827264207b3db93bc2c23
abubakrmo <abubakarmufutauoseni@gmail.com>
2024-05-16 11:26:46 +0100

    createblog form template created

--- src/createblog.js
+++ src/createblog.js
@@ -1,7 +1,26 @@
+import { useState } from "react";
+
 const CreateBlog = () => {
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
+    const [author, setAuthor] = useState('')
+
+
     return ( 
         <div className="create-blog">
             <h2 style={{textAlign: 'start'}}>Create new blog</h2>
+            <form action="">
+                <input 
+
+                />
+                <textarea
+                ></textarea>
+                <select>
+                    <option value="Benjamin Pavard">Benjamin Pavard</option>
+                    <option value="Oglos Danielle">Oglos Danielle</option>
+                </select>
+                <button type="submit">Create</button>
+            </form>
         </div>
      );
 }
