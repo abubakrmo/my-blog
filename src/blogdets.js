a68337991381525b937a4968f06ddd225d9e82aa
abubakrmo <abubakarmufutauoseni@gmail.com>
2024-05-23 16:40:43 +0100

    logic and template to delete blog post created

--- src/blogdets.js
+++ src/blogdets.js
@@ -1,9 +1,19 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import { useNavigate } from "react-router-dom";
 const BlogDetails = () => {
     const { id } = useParams()
 
     const {data: blog, isLoading, error} = useFetch('http://localhost:8000/blogs/' + id)
+    const navigate = useNavigate();
+
+    const handleDelete = () =>{
+        fetch('http://localhost:8000/blogs/' + id, {
+            method: 'DELETE'
+        }).then(()=>{
+            navigate('/')
+        })
+    }
     return ( 
         <div className="blog-details">
             {isLoading && <div style={{textAlign: "start"}}>Loading...</div>}
@@ -14,6 +24,7 @@ const BlogDetails = () => {
                    <h2>{blog.title}</h2>
                    <h4>{blog.author}</h4>
                    <p>{blog.body}</p>
+                   <button onClick={handleDelete}>Delete</button>
                 </article>
 
             )} 
